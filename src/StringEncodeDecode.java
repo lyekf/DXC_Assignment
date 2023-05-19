67b2be7c9f3e70326a43e3d81e66e27b3e2795bd
lyekf <lyekf@hotmail.com>
2023-05-19 10:21:41 +0800

    Commit project 2

--- src/StringEncodeDecode.java
+++ src/StringEncodeDecode.java
@@ -73,71 +73,7 @@ public class StringEncodeDecode {
         return decodedStr;
 
     }
-/*
-    public String encode(String plainText) {
-        int len = plainText.length();
 
-        char chbuf[] = new char[len - 1];
-
-        char offsetchar = plainText.charAt(0);
-        int offsetchar_tblindex = str_reference_tbl.indexOf(offsetchar);
-
-        for (int i = 1; i < len; i++) {
-
-            char currentchar = plainText.charAt(i);
-            int currentchar_tblindex = str_reference_tbl.indexOf(currentchar);
-
-            if (currentchar_tblindex == -1) {    //this character in plaintext is not found in reference table
-                chbuf[i - 1] = currentchar;                //map back to same character
-            } else {
-                if (currentchar_tblindex - offsetchar_tblindex < 0) {
-                    chbuf[i - 1] = str_reference_tbl.charAt(str_reference_tbl_Len - (offsetchar_tblindex - currentchar_tblindex));
-                } else {
-                    chbuf[i - 1] = (char) (currentchar_tblindex - offsetchar_tblindex + 'A');
-                }
-            }
-
-        }
-        String encodedStr = "" + offsetchar + new String(chbuf);
-        return encodedStr;
-
-
-    }
-*/
-/*
-    public String decode(String encodedText) {
-
-        int len = encodedText.length();
-
-        char chbuf[] = new char[len - 1];
-
-        char offsetchar = encodedText.charAt(0);
-        int offsetchar_tblindex = str_reference_tbl.indexOf(offsetchar);
-
-        for (int i = 1; i < len; i++) {
-
-            char currentchar = encodedText.charAt(i);
-            int currentchar_tblindex = str_reference_tbl.indexOf(currentchar);
-
-            if (currentchar_tblindex == -1) {    //this character in plaintext is not found in reference table
-                chbuf[i - 1] = currentchar;                //map back to same character
-            } else {
-                if (currentchar_tblindex + offsetchar_tblindex < str_reference_tbl_Len) {
-                    chbuf[i - 1] = str_reference_tbl.charAt(currentchar_tblindex + offsetchar_tblindex);
-                } else {
-                    chbuf[i - 1] = str_reference_tbl.charAt(offsetchar_tblindex - (str_reference_tbl_Len - currentchar_tblindex));
-                }
-
-            }
-        }
-
-        String decodedStr = new String(chbuf);
-
-
-        return decodedStr;
-
-    }
-*/
 
     public static void main(String[] args) {
 
@@ -149,7 +85,7 @@ public class StringEncodeDecode {
         String plainText = scanner.nextLine();
 
         System.out.print("Enter offset character :");
-        char offsetchar = scanner.nextLine().charAt(0);
+        char offsetchar = scanner.nextLine().charAt(0);     //offsetchar is assumed to be valid in the reference table
         String offsetstr = "" + offsetchar;
         plainText = offsetstr + plainText;
 
